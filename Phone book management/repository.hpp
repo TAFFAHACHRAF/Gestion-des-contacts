fc80b552a25f203e84c75de7d582b891e2770fbc
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-04-08 00:41:58 +0200

    Add files via upload

---
+++ Phone book management/repository.hpp
@@ -0,0 +1,11 @@
+#ifndef _repository_t_
+	#define _repository_t_
+	#include"listeContact.hpp"
+	class repository{
+		public:
+			listContact *llist;
+			repository_t(){
+				llist=NULL;
+			}
+	};
+#endif
