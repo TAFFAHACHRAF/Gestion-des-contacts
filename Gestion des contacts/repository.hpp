a9e5f9a546797f7d3a90dce3dadd5f21b5bf777a
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-04-09 02:09:47 +0200

    Update repository.hpp

--- Gestion des contacts/repository.hpp
+++ Gestion des contacts/repository.hpp
@@ -2,15 +2,19 @@
 	#define _repository_t_
 	#include"listeContact.hpp"
 	class repository{
-		listContact llist[27];
+		listContact listes[27];
 		public:
-			repository_t(){
+			repository(){
 				for(int i=0;i<26;i++){
-					llist[i]=listContact();
+					listes[i]=listContact();
 				}
 			}
-			void AddContact(){
-				
+			void AddContact(string nom,string tel,string email){
+			    int index = toupper(nom[0])-65;
+				listes[index].AddContact(nom,tel,email);
+			}
+			void Display(){
+				listes[1].display();
 			}
 	};
 #endif
