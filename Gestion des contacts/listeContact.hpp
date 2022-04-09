7cb1a3eb4599a5fd6e0f9e1e977094b26e45f423
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-04-09 02:12:40 +0200

    Update listeContact.hpp

--- Gestion des contacts/listeContact.hpp
+++ Gestion des contacts/listeContact.hpp
@@ -16,17 +16,62 @@
 			void AddContact(string nom,string tel,string email){
 				contact *c=new contact(nom,tel,email);
 				node *last=new node(c);
-				if(head==NULL)
+				if(head==NULL){
 					head=last;
+					head->next=NULL;
+					head->previous=NULL;
+				}
 				else{
 					node *temp=new node();
 					temp=head;
 					while(temp->next!=NULL){
 						temp=temp->next;
 					}
+					temp->previous=temp;
 					temp->next=last;
 				}
 			}
+			void DeleteLast(){
+				if(head==NULL || head->next==NULL){
+					head=NULL;
+				}
+				else{
+					node *temp=new node();
+					temp=head;
+					while(temp->next->next!=NULL){
+						temp=temp->next;
+					}
+					temp->next=NULL;
+				}	
+			}
+			void DeleteFirst(){
+				if(head==NULL || head->next==NULL){
+					head=NULL;
+				}
+				else{
+					node *temp=new node();
+					temp=head->next;
+					head=temp;
+				}
+			}
+//			void DeleteByNom(string nom){
+//				node *result=new node();
+//				result=getByNom(nom);
+//				if(result==NULL || result->next==NULL){
+//					result=NULL;
+//				}
+//				else{
+//					node *temp=new node();
+//					temp=head;
+//					while(temp!=NULL){
+//						if(temp->next==result){
+//							temp->next=temp->next->next;
+//							return;
+//						}
+//						temp=temp->next;
+//					}
+//				}
+//			}
             node *getByNom(string nom){
             	if(head->data->getNom()==nom)
             	   return head;
@@ -104,7 +149,7 @@
 			void display(){
 				node *temp=new node(head);
 				if(temp==NULL){
-					return;
+					cout << "Rien a afficher";
 				}
 				else{
 					while(temp!=NULL){
